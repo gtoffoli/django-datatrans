75b502d40a977c77767cf8b3456cb53b03a17d41
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2013-04-11 11:32:05 +0200

    autodiscover

--- datatrans/__init__.py
+++ datatrans/__init__.py
@@ -1,4 +1,5 @@
-VERSION = (0, 1, 3)
+VERSION = (0, 1, 4)
+# 1.3 is currently on pypi
 
 
 def get_version():
@@ -9,3 +10,32 @@ def get_version():
 
 
 __version__ = get_version()
+
+
+def autodiscover():
+    """
+    Same principle as for importing the admin modules with autodiscover() from django.contrib.admin
+    """
+    import copy
+    import logging
+    from django.conf import settings
+    from django.contrib.admin.sites import site
+    from django.utils.importlib import import_module
+    from django.utils.module_loading import module_has_submodule
+
+    logger = logging.getLogger(__name__)
+
+    for app in settings.INSTALLED_APPS:
+        mod = import_module(app)
+        before_import_registry = copy.copy(site._registry)
+
+        try:
+            import_module('%s.datatranslation' % app)
+            logger.info("Imported %s.datatranslation" % app)
+        except:
+            site._registry = before_import_registry
+
+            if module_has_submodule(mod, 'datatranslation'):
+               raise SystemError('Oh snap, weird stuff going on. We detected a submodule called \'datatrans\' in %s' % app)
+
+
