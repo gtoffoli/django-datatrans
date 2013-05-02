7328574d01685bba1f916a1f62d61c44366ec4fe
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2013-05-02 11:16:02 +0200

    logging

--- datatrans/models.py
+++ datatrans/models.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 from django.conf import settings
 from django.core.cache import cache
 from django.db import models
@@ -8,6 +9,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
 from hashlib import sha1
+
+logger = logging.getLogger(__name__)
 
 
 def make_digest(key):
@@ -45,6 +48,12 @@ class KeyValueManager(models.Manager):
 
     def lookup(self, key, language, obj, field):
         kv = self.get_keyvalue(key, language, obj, field)
+
+        # Temporary logging for tracking a weird bug
+        # TODO: delete this!
+        if isinstance(kv, int):
+            logger.warning('Oh no, the KeyValue is an int %s, %s, %s, %s', key, language, obj, field)
+
         if kv.edited:
             return kv.value
         else:
