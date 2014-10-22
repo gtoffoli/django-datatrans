5d0b1ea894f0bd298022ae9acbe47931c4b830b2
Niels Van Och <niels.vanoch@gmail.com>
2014-10-22 08:49:24 +0200

    Don't mask errors with non-useful one

--- datatrans/__init__.py
+++ datatrans/__init__.py
@@ -16,19 +16,11 @@ def autodiscover():
     """
     Same principle as for importing the admin modules with autodiscover() from django.contrib.admin
     """
-    import logging
     from django.conf import settings
     from django.utils.importlib import import_module
-    from django.utils.module_loading import module_has_submodule
-
-    logger = logging.getLogger(__name__)
 
     for app in settings.INSTALLED_APPS:
-        mod = import_module(app)
-
         try:
             import_module('%s.datatranslation' % app)
-            logger.info("Datatrans imported '%s.datatranslation'" % app)
-        except:
-            if module_has_submodule(mod, 'datatranslation'):
-               raise SystemError('Oh snap, weird stuff going on. We detected a submodule called \'datatranslation\' in %s' % app)
+        except ImportError:
+            pass
